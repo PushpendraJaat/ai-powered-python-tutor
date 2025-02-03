5a64ff3347d4afea05c7625c9d2d46cf7b791e5d
Pushpendra Jaat <pushpendrajaat2001@gmail.com>
2025-02-03 11:44:27 +0530

    refactor: simplify authentication error messages and update token handling in NextAuth configuration

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,7 +4,6 @@ import bcrypt from "bcrypt";
 import dbConnect from "@/lib/dbConnect";
 import { User } from "@/models/User";
 
-
 const authOptions: NextAuthOptions = {
   providers: [
     CredentialsProvider({
@@ -17,31 +16,22 @@ const authOptions: NextAuthOptions = {
         if (!credentials?.email || !credentials?.password) {
           throw new Error("Missing email or password");
         }
-
         await dbConnect();
-
         try {
           const user = await User.findOne({ email: credentials.email });
           if (!user) {
-            throw new Error("User not found with given email");
+            throw new Error("user not found");
           }
-
           const isPasswordValid = await bcrypt.compare(credentials.password, user.password);
           if (!isPasswordValid) {
-            throw new Error("User password is not valid");
+            throw new Error("password invalid");
           }
-
-          const userWithId = {
-            ...user,
+          // Return user data that will be saved in the JWT. Ensure _id is a string.
+          return {
             id: user._id.toString(),
             email: user.email,
             name: user.name,
-            role: user.role,
           };
-
-          // Return user data that will be saved in the JWT.
-          return userWithId
-
         } catch (error) {
           console.error("Error in auth:", error);
           return null;
@@ -56,7 +46,7 @@ const authOptions: NextAuthOptions = {
   callbacks: {
     async jwt({ token, user }) {
       if (user) {
-        token.id = user.id;
+        token._id = user.id;
       }
       return token;
     },
@@ -65,7 +55,7 @@ const authOptions: NextAuthOptions = {
         ...session,
         user: {
           ...session.user,
-          id: token.id,
+          id: token._id,
         }
       };
     }
@@ -73,23 +63,6 @@ const authOptions: NextAuthOptions = {
   pages: {
     signIn: "/auth/signin",
   },
-  cookies: {
-    sessionToken: {
-      name: 'next-auth.session-token',
-      options: {
-        domain: process.env.NEXTAUTH_URL?.includes("vercel.app")
-        ? "skc-pushpendra-jaat-ai-powered-python-tutor.vercel.app"
-        : undefined, // No domain for local dev
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: 'lax',
-        path: '/',
-      },
-    },
-  },
-  
-  
 };
-
 const handler = NextAuth(authOptions);
-
 export { handler as GET, handler as POST };
